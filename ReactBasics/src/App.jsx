c591bff901208f1f083e1b7e1a50e6b331042615
Saipavanvadapally <pavanvadapally034@gmail.com>
2024-02-17 18:28:40 +0530

    new changes done

--- ReactBasics/src/App.jsx
+++ ReactBasics/src/App.jsx
@@ -1,16 +1,21 @@
-
-import React from 'react';
+import { useState } from "react";
+import React from "react";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { TaskList } from "./components/TaskList";
-import './App.css';
+import "./App.css";
+import { AddTask } from "./components/AddTask";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
 
   return (
     <div className="App">
       <Header />
-      <TaskList />
+      <main>
+        <AddTask tasks={tasks} setTasks={setTasks} />
+        <TaskList tasks={tasks} setTasks={setTasks} />
+      </main>
       <Footer />
     </div>
   );
