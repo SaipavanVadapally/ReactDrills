450ef5cb19c302cbfb2eff650a94d140bea6f2f6
Saipavanvadapally <pavanvadapally034@gmail.com>
2024-02-17 18:27:31 +0530

    Changed Task list with dynamic values using hooks

--- ReactBasics/src/components/TaskList.jsx
+++ ReactBasics/src/components/TaskList.jsx
@@ -5,21 +5,10 @@ import { BoxCard } from './BoxCard';
 import "./TaskList.css";
 import React from 'react';
 
-export const TaskList = () => {
-    const [tasks, setTasks] = useState([
-        {id: 5271, name: "Completed PropDrilling", completed: true}, 
-        {id: 7825, name: " Completed Dynamic Styles Implementation", completed: true}, 
-        {id: 8391, name: "Pending about Hooks", completed: false}
-    ]);
+export const TaskList = ({tasks,setTasks}) => {
+    
     const [show, setShow] = useState(true);
-    const styles = {
-        color: show ? "#3D8361" : "#be3434",
-        border: "2px solid", 
-        borderColor: show ? "#3D8361" : "#be3434" ,
-        borderRadius: "5px",
-        fontSize: "28px",
-        padding: "20px"
-    }
+     
 
     function handleDelete(id){
         setTasks(tasks.filter(task => task.id !== id));
@@ -27,12 +16,16 @@ export const TaskList = () => {
 
   return (
     <section className='tasklist'>
-        <h1 style={styles}>Task List</h1>
+         
         <ul>
-            <button className='tasklist button.trigger' onClick={() => setShow(!show)}>{show?"Hide":"Show"}</button>
+            <div className='header'> 
+            <h1>Task List</h1>
+            <button className=' trigger' onClick={() => setShow(!show)}>{show?"Hide":"Show"}</button>
+            </div>
             { show && tasks.map((task) => (
                 <TaskCard key={task.id} task={task} handleDelete={handleDelete} />
             )) }
+            
         </ul>
         <BoxCard result="success">
             <p className="title">Offer Notification</p>
